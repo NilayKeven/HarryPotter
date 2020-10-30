c1dd18d91df8efbcfab767d28f98aa97c360bff1
Nilay Keven <nilay.keven@dk.akbank.com>
2020-10-30 17:17:40 +0300

    Created animation view for character list

--- HarryPotter/Screens/CharacterList/CharacterListView.swift
+++ HarryPotter/Screens/CharacterList/CharacterListView.swift
@@ -12,7 +12,23 @@ struct CharacterListView: View {
     @ObservedObject var viewModel = CharacterListViewModel()
     
     var body: some View {
-        Text("Hello, world")
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+                ForEach(viewModel.characterList) { character in
+                    GeometryReader(content: { geometry in
+                        WebImage(url: URL(string: character.image ?? ""))
+                            .resizable()
+                            .frame(width: 300, height: 400, alignment: .center)
+                            .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX) / 20),
+                                              axis: (x: 0, y: 1, z: 1))
+                            .onTapGesture {
+                                let rootVC = (UIApplication.shared.windows.first)?.rootViewController as? UINavigationController
+                                rootVC?.pushViewController(UIHostingController(rootView: Text(character.name ?? "")), animated: false)
+                            }
+                    }).frame(width: 300, height: 400, alignment: .center)
+                }
+            }).padding()
+        })
     }
 }
 
